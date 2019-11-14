07682f3ce31c57e3adbc82e832985df9a72dbfd7
seven <1957781657@qq.com>
2019-11-14 10:45:48 +0800

    generator

---
+++ generator_test/src/main/java/com/caiba/mapper/VideoMapper.java
@@ -0,0 +1,17 @@
+package com.caiba.mapper;
+
+import com.caiba.entity.Video;
+
+public interface VideoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Video record);
+
+    int insertSelective(Video record);
+
+    Video selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Video record);
+
+    int updateByPrimaryKey(Video record);
+}
